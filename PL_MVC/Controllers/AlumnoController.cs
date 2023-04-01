c545afb1fd35fb75ab2c1ea723b82cdc5710d6c0
iespinoza96 <iespinoza@digis01.com>
2023-03-31 18:34:51 -0600

    GetAll y Form

--- PL_MVC/Controllers/AlumnoController.cs
+++ PL_MVC/Controllers/AlumnoController.cs
@@ -14,17 +14,34 @@ namespace PL_MVC.Controllers
             ML.Result result = BL.Alumno.GetAllEF();
             ML.Alumno alumno = new ML.Alumno();
 
+
             if (result.Correct)
             {
                 alumno.Alumnos = result.Objects;
+               
             }
             else
             {
-                ViewBag.Message = "Ocurrio un error al hacer la consulta de alumnos";
+                ViewBag.Message = "Ocurrio un error al hacer la consulta de alumnos" + result.ErrorMessage;
             }
 
-
             return View(alumno);
+        }
+
+        public ActionResult Form(int? idAlumno) 
+        {
+            //add o actualizar 
+            if (idAlumno == null)
+            {
+                //add
+            }
+            else 
+            {
+                //getbyid(idAlumno)
+                //actualizar
+
+            }
+            return View();
         }
     }
 }
